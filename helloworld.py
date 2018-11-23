ad181d70fb9a5801a04b6c39e07ea4bf366df542
A. Younis <ayounis.ext@orange.com>
2018-11-23 19:39:16 +0200

    helloworld.py changed

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,19 @@
 print("connect four")
 x = input("please enter the column number:")
 
-def board(x):
-    for i in range(x):
-        print i
+raw1={0,0,0,0,0,0,0,0}
+raw2={0,0,0,0,0,0,0,0}
+raw3={0,0,0,0,0,0,0,0}
+raw4={0,0,0,0,0,0,0,0}
+raw5={0,0,0,0,0,0,0,0}
+raw6={0,0,0,0,0,0,0,0}
+raw7={0,0,0,0,0,0,0,0}
+raw8={0,0,0,0,0,0,0,0}
 
-board(x)
+
+def board():
+    for i in range(8):
+        print raw+i
+
+board()
 
