dba0a4553398819b2166e7f1a35dec59771d51ef
A. Younis <ayounis.ext@orange.com>
2018-11-25 10:09:39 +0200

    pla pla pla

--- helloworld.py
+++ helloworld.py
@@ -1,19 +1,12 @@
 print("connect four")
-x = input("please enter the column number:")
 
-raw1={0,0,0,0,0,0,0,0}
-raw2={0,0,0,0,0,0,0,0}
-raw3={0,0,0,0,0,0,0,0}
-raw4={0,0,0,0,0,0,0,0}
-raw5={0,0,0,0,0,0,0,0}
-raw6={0,0,0,0,0,0,0,0}
-raw7={0,0,0,0,0,0,0,0}
-raw8={0,0,0,0,0,0,0,0}
+row={"0","0","0","0","0","0","0","0"}
 
+print(' '.join(row) )
 
 def board():
-    for i in range(8):
-        print raw+i
+    for i in range(8) :
+        print("hellow")
 
 board()
 
